2ee49794c2977b66d0fe0c3db937b3b60cf0b2e1
Prakhar Vaish <73419181+VAISHPrakhar@users.noreply.github.com>
2024-01-24 15:38:50 +0530

    Update weather.py

--- TASK 3/weather.py
+++ TASK 3/weather.py
@@ -9,7 +9,7 @@ def weather(city):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Accept-Language': 'en-US,en;q=0.9'  # Setting language preference to English
     }
-    
+
     res = requests.get(url, headers=headers)
     print("Searching...\n")
     
